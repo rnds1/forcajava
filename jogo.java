728109d3fc16711c34ff180b2f9ef752fc13a3fc
rnds1 <42522990+rnds1@users.noreply.github.com>
2019-09-01 17:59:12 -0300

    Update jogo.java

---
+++ jogo.java
@@ -0,0 +1,36 @@
+public class Jogo{
+	private String palavra;
+	public String mascara;
+	private int chances = 6;
+	
+	
+	//define a mascara
+	public void setMascara(){
+		for(int i = 0;i < palavra.lenght; i++){
+			marcara.charAt(i) = "-";
+		}
+	}
+	public void mostrarMascara(){
+		System.out.println(mascara);
+	}
+	
+	public void mostrarForca(){
+	/*
+	
+	"|||||||"
+	"|   o"
+	"|  /|\"
+	"| / | \"
+	"|   |"
+	"|  / \"
+	"| /   \"
+	
+	*/
+	}
+	public void jogadaN(){}
+	
+	
+	
+	
+	
+}
