ffc24e6740131212ebc9f90ddd5e21ba41865ebf
rnds1 <42522990+rnds1@users.noreply.github.com>
2019-09-07 22:27:08 -0300

    aprendendo
    Aula de hj converter String para char e vice verça;

--- controle.java
+++ controle.java
@@ -1,17 +1,28 @@
+
+
+
+
 public class Controle{
-	
+		
 	public static void main(String []arg){
+		char b = 'a';
+		
 		//cria nova partida;
 		Jogo partida = new Jogo();
+			partida.jogar();
+			//debug
 			partida.setMascara();
+			
+			//motd
+			System.out.println("Bem vindo");
+			
+			//in-game DebugGraphics
+			partida.mostrarForca(7);
+			//in-game Debug
+			
+			//partida.jogada(b);
 		//enquanto houverem chances
-		while(partida.chances< 7){
-			mostrarMascara()
-			mostrarForca();
 		
-			
-			
-		}
 		
 		
 		
--- jogo.java
+++ jogo.java
@@ -1,48 +1,52 @@
+
 public class Jogo{
 	private String palavra;
 	private String mascara;
 	public int chances = 0;
+	private int w;
+	public String ex[]	 = {
+	"|||||||",
+	"|   o",
+	"|  /|\\",
+	"| / | \\",
+	"|   |",
+	"|  / \\",
+	"| /   \\"};
+	//funcionando
 	
-	
+	public void jogar(){
+		this.palavra = "abc";
+		//funcionando
+		//System.out.println("AFB:"+ this.palavra);
+		
+	}
 	//define a mascara
 	public void setMascara(){
-		for(int i = 0;i < palavra.length; i++){
-			marcara.charAt(i) = "-";
-		}
+		this.mascara = this.palavra;
+		
+		System.out.println("mascara definida:"+mascara);
 	}
 	public void mostrarMascara(){
-		System.out.println(mascara);
+		System.out.println("mostrando mascara");
 	}
 	
-	public void mostrarForca(){
-	enxtension[][] = [
-	{"|||||||"},
-		{"|   o"},
-			{"|  /|\\"},
-				{"| / | \\"},
-					{"|   |"},
-						{"|  / \\"},
-							{"| /   \\"}
+	public void mostrarForca(int x){
 	
-	];
-	/*
-	
-	"|||||||"
-	"|   o"
-	"|  /|\"
-	"| / | \"
-	"|   |"
-	"|  / \"
-	"| /   \"
-	
-	*/
-	for( int x =0;x<chances;x++){
-			system.out.println(extension[i]);
+		for(this.w =0;this.w < x; this.w ++){ 
+	System.out.println(this.ex[this.w]);
 		}
+	
+	
 	}
 	
 	
-	public void jogadaN(){}
+	public void jogadaN(char x){
+		
+		if(x == palavra.charAt(0)){
+
+		}
+		
+	}
 	
 	
 	
