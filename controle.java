8dec5abd5cbc74167748fbb997f665d246d4be48
rnds1 <42522990+rnds1@users.noreply.github.com>
2019-09-08 12:11:17 -0300

    Looperror

--- controle.java
+++ controle.java
@@ -1,24 +1,41 @@
 
+import java.util.Scanner;
+
+
 
 
 
 public class Controle{
 		
-	public static void main(String []arg){
-		char b = 'a';
+	public static void main (String []arg){
+            Scanner ler = new Scanner(System.in);
+                String li;
+                char l[];
+		
 		
 		//cria nova partida;
 		Jogo partida = new Jogo();
 			partida.jogar();
 			//debug
-			partida.setMascara();
+                      // partida.mostrarForca(7);
+			//partida.setMascara();
 			
 			//motd
-			System.out.println("Bem vindo");
+			//System.out.println("Bem vindo");
 			
 			//in-game DebugGraphics
-			partida.mostrarForca(7);
+			
+                        
+                      // partida.jogadaN((char) l[0]);
+                    
 			//in-game Debug
+                        while(partida.chances> 0){
+                           li= ler.nextLine();
+                        //System.out.println(li);
+                        l = li.toCharArray();
+                        //System.out.println(l[0]);
+                               
+                        }
 			
 			//partida.jogada(b);
 		//enquanto houverem chances
--- jogo.java
+++ jogo.java
@@ -1,8 +1,10 @@
 
 public class Jogo{
+    
+        
 	private String palavra;
 	private String mascara;
-	public int chances = 0;
+	public int chances = 6;
 	private int w;
 	public String ex[]	 = {
 	"|||||||",
@@ -11,26 +13,42 @@ public class Jogo{
 	"| / | \\",
 	"|   |",
 	"|  / \\",
-	"| /   \\"};
+	"| /   \\\n\n"};
 	//funcionando
 	
 	public void jogar(){
-		this.palavra = "abc";
+		this.palavra = "abcdef";
 		//funcionando
+                
 		//System.out.println("AFB:"+ this.palavra);
 		
+                
 	}
 	//define a mascara
 	public void setMascara(){
-		this.mascara = this.palavra;
-		
-		System.out.println("mascara definida:"+mascara);
+            
+            //Funcionando
+		//this.mascara = this.palavra;
+		int i = this.palavra.length();
+                char[] b = this.palavra.toCharArray();
+                for(int x =0;x<i;x++){
+                //HERE chance all characters 
+                b[x] = '-';
+                    
+                }
+                this.mascara = String.valueOf(b);
+		System.out.println(mascara);
 	}
-	public void mostrarMascara(){
-		System.out.println("mostrando mascara");
-	}
+        
+	/*public void mostrarMascara(){
+            System.out.println(mascara);
+            
+            //done
+		//System.out.println("mostrando mascara");
+	}*/
 	
 	public void mostrarForca(int x){
+            //Funcionando
 	
 		for(this.w =0;this.w < x; this.w ++){ 
 	System.out.println(this.ex[this.w]);
@@ -41,14 +59,39 @@ public class Jogo{
 	
 	
 	public void jogadaN(char x){
-		
-		if(x == palavra.charAt(0)){
-
-		}
-		
-	}
+                if(this.chances <= 0){
+                
+                }
+            
+                //Funcionando
+                boolean contem = false;
+		char[] b = this.palavra.toCharArray();
+                int i,max;
+                max = this.palavra.length();
+              for(i=0;i<max;i++){
+		if(b[i] == x  ){
+                    contem = true;
+                    this.subistituir(i, x);
+                    //System.out.println("xfx");
+                    
+                    }
+                
+                
+                
+                
+              } //end for
+		if(contem == false){
+                this.chances = this.chances - 1;
+                }
+	}//end void
 	
-	
+	public void subistituir(int pox, char c){
+            char[] b = this.mascara.toCharArray();
+            b[pox] = c;
+            this.mascara = String.valueOf(b);
+            
+        
+        }
 	
 	
 	
